ad0ba89cb7cf892c1b999c94eb82a8a9d43f5c64
Ivan Milaev <milaevivan@gmail.com>
2019-05-31 13:25:19 +0300

    Initial Commit

---
+++ cppstudio/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  cppstudio
+//
+//  Created by Иван Милаев on 31/05/2019.
+//  Copyright © 2019 IvanMilaev. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
